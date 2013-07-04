ab9529a9fe9a248fc4227bff0092fa16bde290e9
Travis Mosley <travis@travismosley.com>
2013-07-04 20:53:41 +0000

    initial commit of startup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git checkout https://github.com/tlm2021/devenv.git
+ln -sb devenv/.tmux.conf ~
+ln -sb devenv/.bash_profile ~
+ln -sb devenv/.bashrc ~
+ln -sb devenv/.bashrc_custom ~
+ln -sb devenv/.gitconfig ~
+ln -sb devenv/.jshintrc ~
+ln -sd .vim devenv/.vim 
+
