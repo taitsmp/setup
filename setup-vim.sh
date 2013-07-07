16771f4509b9b123e65c56ef87179fefc03caf88
Tait Larson <tait@stylemepretty.com>
2013-07-07 17:22:59 +0000

    added slime.vim

--- setup-vim.sh
+++ setup-vim.sh
@@ -7,8 +7,10 @@ curl -Sso ~/.vim/autoload/pathogen.vim \
 
 #jshint
 git clone https://github.com/Shutnik/jshint2.vim.git ~/.vim/bundle/jshint2.vim/
+git clone git://github.com/jpalardy/vim-slime.git ~/.vim/bundle/vim-slime/
 
 echo Add the following to your .vimrc file to get pathogen to work.
 echo "execute pathogen#infect()"
+echo 'let g:slime_target = "tmux"'
 
 #TODO: automate .vimrc creation
