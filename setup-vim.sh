9bc3c8fa53c808daf2d805852a78659c5e3f8833
Tait Larson <tait@stylemepretty.com>
2013-07-07 11:31:34 +0000

    vim setup script

--- setup-vim.sh
+++ setup-vim.sh
@@ -8,4 +8,7 @@ curl -Sso ~/.vim/autoload/pathogen.vim \
 #jshint
 git clone https://github.com/Shutnik/jshint2.vim.git ~/.vim/bundle/jshint2.vim/
 
+echo Add the following to your .vimrc file to get pathogen to work.
+echo "execute pathogen#infect()"
 
+#TODO: automate .vimrc creation
