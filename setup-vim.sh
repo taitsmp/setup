7abdd72ccc76c60f298b93b15643036265d80e89
Tait Larson <tait@stylemepretty.com>
2014-01-08 20:30:18 -0500

    Update setup-vim.sh
    User for personal setup (not startup class)

--- setup-vim.sh
+++ setup-vim.sh
@@ -9,6 +9,9 @@ curl -Sso ~/.vim/autoload/pathogen.vim \
 git clone https://github.com/Shutnik/jshint2.vim.git ~/.vim/bundle/jshint2.vim/
 git clone git://github.com/jpalardy/vim-slime.git ~/.vim/bundle/vim-slime/
 
+#cucumber / yadda
+git clone https://github.com/tpope/vim-cucumber.git ~/.vim/bundle/vim-cucumber/
+
 echo Add the following to your .vimrc file to get pathogen to work.
 echo "execute pathogen#infect()"
 echo 'let g:slime_target = "tmux"'
