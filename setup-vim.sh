b7e4f475c36d9868cc9b623fa4b00c2cba79a82a
Tait Larson <tait@stylemepretty.com>
2014-02-02 20:04:24 -0500

    Update setup-vim.sh

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#pathogen
+#install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
