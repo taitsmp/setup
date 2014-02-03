e85ff0f7c0854cf5d3008d7fdf2efb1f51093ae9
Tait Larson <tait@stylemepretty.com>
2014-02-02 20:08:53 -0500

    .vimrc created by dotfiles repo.

--- setup-vim.sh
+++ setup-vim.sh
@@ -12,8 +12,4 @@ git clone git://github.com/jpalardy/vim-slime.git ~/.vim/bundle/vim-slime/
 #cucumber / yadda
 git clone https://github.com/tpope/vim-cucumber.git ~/.vim/bundle/vim-cucumber/
 
-echo Add the following to your .vimrc file to get pathogen to work.
-echo "execute pathogen#infect()"
-echo 'let g:slime_target = "tmux"'
 
-#TODO: automate .vimrc creation
