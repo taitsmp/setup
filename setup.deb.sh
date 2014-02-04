6d9bb430578f32cd6a8c2f1a83cc08123b8c0a3e
Tait Larson <telarson@gmail.com>
2014-02-03 21:04:45 -0500

    refactored out a few things. should be easier to setup vim, tmux, etc. on centos and osx.

---
+++ setup.deb.sh
@@ -0,0 +1,12 @@
+
+#debian or ubuntu setup files. 
+
+# Install rlwrap. Required for node in tmux
+sudo apt-get install -y rlwrap
+
+# Install node, since I'm using it a lot
+sudo apt-get install -y nodejs
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+npm install -g jshint
--- setup.sh
+++ setup.sh
@@ -4,21 +4,9 @@ git clone https://github.com/tlm2021/devenv.git
 
 ./setup-vim.sh
 
-ln -sb devenv/.tmux.conf ~
-ln -sb devenv/.bash_profile ~
-ln -sb devenv/.bashrc ~
-ln -sb devenv/.bashrc_custom ~
+ln -sb dotfiles/.tmux.conf ~
+ln -sb dotfiles/.bash_profile ~
+ln -sb dotfiles/.bashrc ~
+ln -sb dotfiles/.bashrc_custom ~
+ln -sb dotfiles/.tmux.conf ~
 
-#ln -sb devenv/.gitconfig ~
-#ln -sb devenv/.jshintrc ~
-#ln -sd .vim devenv/.vim 
-
-# Install rlwrap. Required for node in tmux
-sudo apt-get install -y rlwrap
-
-# Install node, since I'm using it a lot
-sudo apt-get install -y nodejs
-
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-npm install -g jshint
