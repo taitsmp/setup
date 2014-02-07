28e522a4c8ff2f4138ade0e34f4c72f642c0101a
Tait Larson <tait@stylemepretty.com>
2014-02-07 13:07:34 -0500

    works better on mac.  includes bash_prompt

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
-# Fetch and link to config files
-# Linking lets me commit changes I make easily
-git clone https://github.com/tlm2021/devenv.git
 
 ./setup-vim.sh
 
-ln -sb dotfiles/.tmux.conf ~
-ln -sb dotfiles/.bash_profile ~
-ln -sb dotfiles/.bashrc ~
-ln -sb dotfiles/.bashrc_custom ~
-ln -sb dotfiles/.tmux.conf ~
+ln -is dotfiles/.tmux.conf ~
+ln -is dotfiles/.bash_profile ~
+ln -is dotfiles/.bashrc ~
+ln -is dotfiles/.bashrc_custom ~
+ln -is dotfiles/.bash_prompt ~
+ln -is dotfiles/.tmux.conf ~
 
