b91f853aa2971cf639654f2440a32157ddbdad11
Tait Larson <telarson@gmail.com>
2014-02-12 08:16:16 -0500

    trying to get editorConfig to work.

--- setup-vim.sh
+++ setup-vim.sh
@@ -8,6 +8,7 @@ curl -Sso ~/.vim/autoload/pathogen.vim \
 #jshint
 git clone https://github.com/Shutnik/jshint2.vim.git ~/.vim/bundle/jshint2.vim/
 git clone git://github.com/jpalardy/vim-slime.git ~/.vim/bundle/vim-slime/
+git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/editorconfig-vim/
 
 #cucumber / yadda
 git clone https://github.com/tpope/vim-cucumber.git ~/.vim/bundle/vim-cucumber/
