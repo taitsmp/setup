aea664a2cc16921eb73484c2c88b1e520f5d3f4d
Tait Larson <telarson@gmail.com>
2014-03-11 09:33:48 -0400

    better git ctags.

--- setup-vim.sh
+++ setup-vim.sh
@@ -8,6 +8,8 @@ curl -Sso ~/.vim/autoload/pathogen.vim \
 #jshint
 git clone https://github.com/Shutnik/jshint2.vim.git ~/.vim/bundle/jshint2.vim/
 git clone git://github.com/jpalardy/vim-slime.git ~/.vim/bundle/vim-slime/
+
+git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive.git
 git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/editorconfig-vim/
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim/
 
--- setup.sh
+++ setup.sh
@@ -11,6 +11,47 @@ ln -is dotfiles/.screenrc ~
 
 #todo: what about emacs?
 
+# global git templates. 
+git config --global init.templatedir '~/.git_template'
+mkdir -p ~/.git_template/hooks
+
+# start git auto ctags
+# http://tbaggery.com/2011/08/08/effortless-ctags-with-git.html
+tee ~/.git_template/hooks/ctags << 'EOF'
+#!/bin/sh
+set -e
+PATH="/usr/local/bin:$PATH"
+trap "rm -f .git/tags.$$" EXIT
+ctags --tag-relative -Rf.git/tags.$$ --exclude=.git --languages=-javascript,sql
+mv .git/tags.$$ .git/tags
+EOF
+
+H1=$(cat <<'DONE'
+#!/bin/sh
+.git/hooks/ctags >/dev/null 2>&1 &
+DONE
+)
+
+for I in post-commit post-merge post-checkout
+do
+  echo "$H1" > ~/.git_template/hooks/${I}
+done
+
+tee ~/.git_template/hooks/post-rewrite << 'EOF'
+#!/bin/sh
+case "$1" in
+  rebase) exec .git/hooks/post-merge ;;
+esac
+EOF
+
+chmod 755 ~/.git_template/hooks/ctags
+chmod 755 ~/.git_template/hooks/post-rewrite
+chmod 755 ~/.git_template/hooks/post-commit
+chmod 755 ~/.git_template/hooks/post-merge
+chmod 755 ~/.git_template/hooks/post-checkout
+# end git auto ctags
+
+git config --global alias.ctags '!.git/hooks/ctags'
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
 
 
