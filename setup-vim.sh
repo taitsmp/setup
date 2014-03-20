9dfb8965d6e5b9da4faa6bcf1680fdf1202bad28
Tait Larson <telarson@gmail.com>
2014-03-20 07:02:03 -0400

    added syntastic

--- setup-vim.sh
+++ setup-vim.sh
@@ -12,6 +12,7 @@ git clone git://github.com/jpalardy/vim-slime.git ~/.vim/bundle/vim-slime/
 git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive.git
 git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/editorconfig-vim/
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim/
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic.git
 
 #cucumber / yadda
 git clone https://github.com/tpope/vim-cucumber.git ~/.vim/bundle/vim-cucumber/
