d2ec2d5583c65c1443d009b3fb98d7deae93a5a4
Tait Larson <telarson@gmail.com>
2014-06-28 13:23:10 -0400

    some issues with haskell.

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
 #install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
@@ -13,8 +17,23 @@ git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive.git
 git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/editorconfig-vim/
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim/
 git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic.git
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript.git
+git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx.git
+git clone git://github.com/majutsushi/tagbar ~/.vim/bundle/tagbar 
+# one of the following is giving me issues. 
+#git clone https://github.com/bitc/vim-hdevtools.git ~/.vim/bundle/vim-hdevtools.git
+#git clone git@github.com:lukerandall/haskellmode-vim.git ~/.vim/bundle/haskellmode-vim.git
+
 
 #cucumber / yadda
 git clone https://github.com/tpope/vim-cucumber.git ~/.vim/bundle/vim-cucumber/
 
-
+#haskell
+if command_exists cabal ; then
+  cabal update
+  cabal install hoogle
+  cabal install cabal-install
+	cabal install ghc-mod
+	cabal install hdevtools
+  cabal install pointfree
+fi
