f622353358c21d92f3799193085f3aff909ef29e
Tait Larson <telarson@gmail.com>
2019-02-12 18:30:40 -0500

    gave up on better ctags for php.

--- setup.mac.sh
+++ setup.mac.sh
@@ -11,6 +11,7 @@ brew install wget
 #install the ag command.
 brew install the_silver_searcher
 brew install shellcheck
+brew install ctags 
 
 #npm install -g jsxhint #for react.js.  must sudo?
 #npm install -g STRML/JSXHint
@@ -20,16 +21,3 @@ npm install -g eslint
 npm install -g babel-eslint
 npm install -g eslint-plugin-react
 
-#make haskell work on the mac with Xcode 5.
-#curl -O http://www.haskell.org/platform/ghc-clang-wrapper
-#chmod a+x ghc-clang-wrapper
-#sudo mv ghc-clang-wrapper /usr/bin/ #yuck
-#sudo /usr/bin/ghc-clang-wrapper
-
-#install - ctags patch for better php parsing
-brew uninstall ctags
-brew doctor
-brew update
-cd /usr/local/Library/Formula
-curl https://gist.githubusercontent.com/taitsmp/9199869/raw/eaaf06d69fb6b5376e97ac5b930f9290b6edae4b/- | git apply
-brew install ctags --HEAD
