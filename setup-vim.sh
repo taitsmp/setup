9317ac53f0aabafaf332ca58948cda7e90e06d0e
TAIT LARSON <taitlarson@MacBook-Pro.fios-router.home>
2019-02-12 16:30:44 -0500

    minor vim setup changes.

--- setup-vim.sh
+++ setup-vim.sh
@@ -5,13 +5,13 @@ command_exists () {
 }
 
 #install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 #jshint
 git clone https://github.com/Shutnik/jshint2.vim.git ~/.vim/bundle/jshint2.vim/
 git clone git://github.com/jpalardy/vim-slime.git ~/.vim/bundle/vim-slime/
+
 
 git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive.git
 git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/editorconfig-vim/
@@ -21,14 +21,9 @@ git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javas
 git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx.git
 git clone git://github.com/burnettk/vim-angular.git ~/.vim/bundle/vim-angular.git
 git clone git://github.com/majutsushi/tagbar ~/.vim/bundle/tagbar 
-git clone git://github.com/zah/nimrod.vim.git ~/.vim/bundle/nimrod
 # one of the following is giving me issues. 
 #git clone https://github.com/bitc/vim-hdevtools.git ~/.vim/bundle/vim-hdevtools.git
 #git clone git@github.com:lukerandall/haskellmode-vim.git ~/.vim/bundle/haskellmode-vim.git
-git clone https://github.com/raichoo/purescript-vim.git ~/.vim/bundle/purescript-vim
-
-#cucumber / yadda
-git clone https://github.com/tpope/vim-cucumber.git ~/.vim/bundle/vim-cucumber/
 
 #haskell
 if command_exists cabal ; then
